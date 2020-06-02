4324cf726f34b5d5c34513a8adacb45051e5955d
batuhanbulutt <66284829+batuhanbulutt@users.noreply.github.com>
2020-06-02 18:36:55 +0300

    Add files via upload

---
+++ hakkı.php
@@ -0,0 +1,29 @@
+
+<html lang="tr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="bıco.png">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <title></title>
+  </head>
+  <body>
+  <div id="page" class="hfeed site">
+  <header class="baslık" role="banner">
+    <hgroup id="title">
+      <h1><a href="index.php" rel="home">batuhanbulut.com</a></h1>
+      <h2>Batuhanın Dünyasına Hoşgeldiniz&#8230;</h2>
+    </hgroup>
+    <div>
+      <div>
+        <button style="background-color:;color:white;width:20%;" type="submit" ><a href="index.php">Anasayfa</a></button>
+        <button style="background-color:;color:white;width:20%;" type="submit" ><a href="hakkı.php">Hakkımda</a></button>
+        <button style="background-color:;color:white;width:20%;" type="submit" ><a href="ıletısım.php">İletişim</a></button>
+        <button style="background-color:;color:white;width:19%;" type="submit" ><a href="kayıtol.php">Kayıt Ol</a></button>
+        <button  type="submit" style="background-color:;color:white;width:19%;" type="submit" ><a href="girisyap.php">Giriş Yap</a></button>
+      </div>
+    </div>
+     <a href="index.php"><img padding:10; src="siber.png" class="baslıkimage" width="1000" height="300"/></a>
+     <p> <h3>BATUHAN BULUT</h3> </p>
+     <p> <h4>Selam, ben Batuhan 21 yaşındayım İstanbul Rumeli Üniversitesinde Bilgisayar Programcılığı bölümünde okuyorum.</h4> </p>
+    </body>
+  </html>
